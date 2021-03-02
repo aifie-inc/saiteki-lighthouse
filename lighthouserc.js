2ca57dae1a9ead0d5f2c3d10a2f4258eb0deb897
kazumoto <motch0214@gmail.com>
2021-03-02 12:13:31 +0900

    fix: Create reports with fixed name

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,10 +5,12 @@ module.exports = {
     collect: {
       url: ["https://lp.saiteki.app"],
       chromePath: puppeteer.executablePath(),
+      numberOfRuns: 1,
     },
     upload: {
       target: "filesystem",
       outputDir: "./reports",
+      reportFilenamePattern: "lp_saiteki-app.%%EXTENSION%%",
     },
   },
 };
