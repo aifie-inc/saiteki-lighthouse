127f54095654fa5bc62afa6a9dc514aabc96b5d5
kazumoto <motch0214@gmail.com>
2022-05-09 07:54:51 +0900

    Config to change domain

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,14 +3,14 @@ const puppeteer = require("puppeteer");
 module.exports = {
   ci: {
     collect: {
-      url: ["https://lp.saiteki.site"],
+      url: ["https://lp.smartlp.site"],
       chromePath: puppeteer.executablePath(),
       numberOfRuns: 1,
     },
     upload: {
       target: "filesystem",
       outputDir: "./reports",
-      reportFilenamePattern: "lp_saiteki-site.%%EXTENSION%%",
+      reportFilenamePattern: "lp_smartlp-site.%%EXTENSION%%",
     },
   },
 };
